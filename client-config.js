97361d426a3130448e34f0ce122fd479c61e9e10
CubeCraftLauncher <abeloskysb2020@gmail.com>
2026-01-11 14:07:19 +0100

    Deploy automatico con CNAME

---
+++ client-config.js
@@ -0,0 +1,6 @@
+// Configuration for the client
+// This file will be overwritten by the build script for production if needed
+window.GAME_CONFIG = {
+    // Production server on Render
+    SERVER_URL: "https://answer-63ef.onrender.com"
+};
